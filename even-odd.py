ac7595bc5bab32a9ccb515ceb484c3226743eb7a
Moamen Sayed <moamen@ymail.com>
2022-06-30 12:27:13 +0200

    refactor checking if string contains letters

--- even-odd.py
+++ even-odd.py
@@ -3,16 +3,19 @@ def extract_evens_odds(string):
     even = []
     odd = []
     for i in string:
-        if int(i) % 2 == 0:
-            even.append(i)
-        if int(i) % 2 == 1:
-            odd.append(i)
-    print("Even numbers:", even)
-    print("Odd numbers:", odd)
+        if i.isnumeric():
+            if int(i) % 2 == 0:
+                even.append(i)
+            if int(i) % 2 == 1:
+                odd.append(i)
+        elif i.isalpha():
+            print(i, "is a letter")
+    # print("Even numbers:", even)
+    # print("Odd numbers:", odd)
     print("Number of Evens:", len(even))
-    print("Number of Odd:", len(odd))
+    print("Number of Odds:", len(odd))
     return "Even numbers:", even, "Odd numbers:", odd
 
 
-print(extract_evens_odds("7894738975984375847589437286184391349"))
+print(extract_evens_odds("jdjd78947389759843jdnfkns75847589437286184391349"))
 
